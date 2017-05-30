7114ee6fece09e98dd8a03a8c02592b0591fe065
Yury Grigoryev <_grigyv@mail.ru>
2017-05-30 19:18:45 +0300

    добавление Greeting

---
+++ src/main/java/Greeting.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Pavilion on 30.05.2017.
+ */
+public class Greeting {
+    public static void main(String...args) {
+        System.out.println("Привет слушателям курса \"Профессиональная разработка на JAVA\"");
+    }
+}
+
